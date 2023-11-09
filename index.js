30547336fd7a7ab75ce250dbf42113496fa0a2bc
marufsiyam <marufhasansiyam6@gmail.com>
2023-11-09 20:18:16 +0600

    make post booking in site

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express =require('express');
-const cors =require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,8 +10,6 @@ app.use(cors());
 app.use(express());
 
 
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ggh3mxu.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,13 +28,30 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-const serviceCollection = client.db('FitnessAndSports').collection('Services');
-app.get('/services', async(req, res) =>{
-  const cursor =serviceCollection.find();
-  const result = await cursor.toArray();
-  res.send(result);
-})
+    const serviceCollection = client.db('FitnessAndSports').collection('Services');
+    const bookingCollection = client.db('FitnessAndSports').collection('bookings')
 
+    app.get('/services', async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options ={
+        projection :{title:1, price:1, service_id:1},
+      }
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result)
+    })
+// booking
+
+app.post('/bookings', async(req, res) =>{
+  const booking =req.body;
+  console.log(booking)
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -49,11 +64,11 @@ app.get('/services', async(req, res) =>{
 run().catch(console.dir);
 
 
-app.get('/',(rew, res) =>{
-    res.send(' the Career Maker Server is runing')
+app.get('/', (rew, res) => {
+  res.send(' the Career Maker Server is runing')
 })
 
 
-app.listen(port, () =>{
-    console.log(`Car Doctor Server is runing on the ${port}`)
+app.listen(port, () => {
+  console.log(`Car Doctor Server is runing on the ${port}`)
 })
